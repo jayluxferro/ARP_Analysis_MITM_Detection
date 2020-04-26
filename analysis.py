c659c7dcac6b6c5a4201a2c966a9c6209509d055
Jay Lux Ferro <jayluxferro@sperixlabs.org>
2020-04-26 17:49:19 +0000

    first analysis failed

--- analysis.py
+++ analysis.py
@@ -5,12 +5,25 @@ Data Analysis
 """
 
 import func as fx
+import sys
 import db
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import LinearSVC, SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import plot_confusion_matrix, plot_roc_curve, plot_precision_recall_curve
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.naive_bayes import *
+
+import logger as lg
 
 # displaying a sample MLS sequence and the RTT
+"""
 binValue = []
 RTT = []
 
@@ -37,3 +50,88 @@ ax2.set_xticks(np.arange(len(x) + 1))
 fig.tight_layout()
 plt.grid(True, axis='both')
 plt.show()
+"""
+
+# inits
+dataset = pd.read_csv(fx.dataset, delimiter=',')
+targets = dataset.iloc[:,1].values
+inputs = dataset.iloc[:,[0]].values
+class_names = ['Normal', 'MITM']
+
+def generateResults(random_state=20):
+    X_train, X_test, y_train, y_test = train_test_split(inputs, targets, test_size=0.2, random_state=random_state)
+    print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+    
+    model = LinearSVC(random_state=0).fit(X_train, y_train)
+    print(model)
+    lg.success('LinearSVC: {:.2f}\n'.format(model.score(X_test, y_test)))
+    plot_confusion_matrix(model, X_test, y_test, normalize='true', display_labels=class_names, cmap=plt.cm.Blues)
+    plt.title('Linear SVC - {:.2f}%'.format(model.score(X_test, y_test)))
+    plt.show()
+
+
+    model = SVC(random_state=0).fit(X_train, y_train)
+    print(model)
+    lg.success('SVC: {:.2f}\n'.format(model.score(X_test, y_test)))
+    plot_confusion_matrix(model, X_test, y_test, normalize='true', display_labels=class_names, cmap=plt.cm.Blues)
+    plt.title('SVC - {:.2f}%'.format(model.score(X_test, y_test)))
+    plt.show()
+
+    model = KNeighborsClassifier(n_neighbors=1).fit(X_train, y_train)
+    print(model)
+    lg.success('KNN: {:.2f}\n'.format(model.score(X_test, y_test)))
+    plot_confusion_matrix(model, X_test, y_test, normalize='true', display_labels=class_names, cmap=plt.cm.Blues)
+    plt.title('KNN - {:.2f}%'.format(model.score(X_test, y_test)))
+    plt.show()
+
+    model = DecisionTreeClassifier(random_state=0).fit(X_train, y_train)
+    print(model)
+    lg.success('DecisionTree: {:.2f}\n'.format(model.score(X_test, y_test)))
+    plot_confusion_matrix(model, X_test, y_test, normalize='true', display_labels=class_names, cmap=plt.cm.Blues)
+    plt.title('Decision Tree - {:.2f}%'.format(model.score(X_test, y_test)))
+    plt.show()
+
+    model = LogisticRegression(random_state=0).fit(X_train, y_train)
+    print(model)
+    lg.success('LogisticRegression: {:.2f}\n'.format(model.score(X_test, y_test)))
+    plot_confusion_matrix(model, X_test, y_test, normalize='true', display_labels=class_names, cmap=plt.cm.Blues)
+    plt.title('Logistic Regression - {:.2f}%'.format(model.score(X_test, y_test)))
+    plt.show()
+    
+    model = RandomForestClassifier(random_state=0).fit(X_train, y_train)
+    print(model)
+    lg.success('RandomForest: {:.2f}\n'.format(model.score(X_test, y_test)))
+    plot_confusion_matrix(model, X_test, y_test, normalize='true', display_labels=class_names, cmap=plt.cm.Blues)
+    plt.title('Random Forest - {:.2f}%'.format(model.score(X_test, y_test)))
+    plt.show()
+
+    model = GradientBoostingClassifier(random_state=0).fit(X_train, y_train)
+    print(model)
+    lg.success('GradientBoosting: {:.2f}\n'.format(model.score(X_test, y_test)))
+    plot_confusion_matrix(model, X_test, y_test, normalize='true', display_labels=class_names, cmap=plt.cm.Blues)
+    plt.title('Gradient Boosting - {:.2f}%'.format(model.score(X_test, y_test)))
+    plt.show()
+    
+    model = GaussianNB().fit(X_train, y_train)
+    print(model)
+    lg.success('GaussianNB: {:.2f}'.format(model.score(X_test, y_test)))
+    plot_confusion_matrix(model, X_test, y_test, normalize='true', display_labels=class_names, cmap=plt.cm.Blues)
+    plt.title('Gassian NB - {:.2f}%'.format(model.score(X_test, y_test)))
+    plt.show()
+    
+    model = BernoulliNB().fit(X_train, y_train)
+    print(model)
+    lg.success('BernoulliNB: {:.2f}'.format(model.score(X_test, y_test)))
+    plot_confusion_matrix(model, X_test, y_test, normalize='true', display_labels=class_names, cmap=plt.cm.Blues)
+    plt.title('Bernoulli NB - {:.2f}%'.format(model.score(X_test, y_test)))
+    plt.show()
+    
+    model = MultinomialNB().fit(X_train, y_train)
+    print(model)
+    lg.success('MultinomialNB: {:.2f}'.format(model.score(X_test, y_test)))
+    plot_confusion_matrix(model, X_test, y_test, normalize='true', display_labels=class_names, cmap=plt.cm.Blues)
+    plt.title('Multinomial NB - {:.2f}%'.format(model.score(X_test, y_test)))
+    plt.show()
+
+# generate linear model results
+generateResults(random_state=20)
--- process_data.py
+++ process_data.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
 """
 Data Analysis
@@ -7,8 +7,12 @@ Data Analysis
 import func as fx
 import db
 import logger as lg
+import matplotlib.pyplot as plt
+import numpy as np
 
 categories = [1, 2]
+normal = []
+mitm = []
 
 lg.default('[-] Generating dataset...')
 fileHandler = open(fx.dataset, 'w')
@@ -18,9 +22,22 @@ for iteration in fx.seqNumbers:
         for data in db.getTableScenarioCategory('incoming', iteration, category):
             for data2 in db.getTableScenarioCategorySeq('outgoing', data['scenario'], category, data['seq']):
                 ctg = category
-                rtt = data['time'] - data2['time']
-                details += '{},{}\n'.format(rtt, ctg)
+                rtt = np.abs(data['time'] - data2['time'])
+                details += '{},{}\n'.format(rtt, ctg-1)
+                if ctg == 1:
+                    normal.append(rtt)
+                else:
+                    mitm.append(rtt)
 
 fileHandler.write(details)
 fileHandler.close()
 lg.success('[+] Dataset generated')
+
+# analyzing the points
+plt.figure()
+plt.plot(np.linspace(1, len(normal), len(normal)), normal, '*')
+plt.plot(np.linspace(1, len(mitm), len(mitm)), mitm, 'o')
+plt.xlabel('Data Points')
+plt.ylabel('RTT')
+plt.legend(['Normal', 'MITM'])
+plt.show()
